70247fd23588d94aec516f5fb4f23be6930c0579
nikhilambhore01 <nikhilamb248@gmail.com>
2021-05-30 11:47:58 +0530

    movie-app-upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import './Header.css';
+
+const Header = () => {
+  return (
+    <span onClick={() => window.scroll(0, 0)} className="header">
+      Prime-Movies-App
+    </span>
+  );
+};
+
+export default Header;
---
+++ src/hooks/useGenre.jsx
@@ -0,0 +1,10 @@
+/** @format */
+
+const useGenre = (selectedGenres) => {
+  if (selectedGenres.length < 1) return '';
+
+  const GenreIds = selectedGenres.map((g) => g.id);
+  return GenreIds.reduce((acc, curr) => acc + ',' + curr);
+};
+
+export default useGenre;
